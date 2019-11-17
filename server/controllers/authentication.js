82fed0397d62e3a4ae21a208379656e50b74796c
Prabhjot Singh Kalra <prabhjotkalra3@gmail.com>
2019-11-17 23:51:52 +0530

    server setup

---
+++ server/controllers/authentication.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat:timestamp }, config.secret);
+}
+
+exports.signup = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(422).send({ error: 'You must enter an email and password' });
+    }
+
+    try {
+        const user = await User.findOne({ email: email });
+     
+        if (user) {
+         return res.status(422).send({error: 'Email already in use'});
+        }
+     
+        const newUser = new User({
+          email,
+          password
+        });
+     
+        try {
+          const savedUser = await newUser.save();
+          res.send({ token: tokenForUser(savedUser) });
+        }
+        catch(err) {
+          return next(err);
+        }
+     
+      }
+      catch(err) {
+        return next(err);
+      }
+}
---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: { type: String, unique: true, lowercase: true },
+    password: String
+});
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(user.password, salt);
+        user.password = hash;
+        next();
+
+    }
+    catch (err) {
+        return next(err);
+    }
+});
+
+
+const ModelClass = mongoose.model('user', userSchema);
+
+module.exports = ModelClass;
